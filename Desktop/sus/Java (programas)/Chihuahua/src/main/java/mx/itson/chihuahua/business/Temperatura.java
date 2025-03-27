ce6f54d33970aaa77ea209826300f24cf0b7b8f4
Andrey-Enrique-GM <you@example.com>
2025-03-27 16:39:48 -0700

    Inicio, ya hay una pestaña y se hicieron 2 metodos, calcular temperatura, que es la principal funcion de este programa y calcular tiempo, que es de otro programa pero teniamos que agregar un segundo metodo aunque no se utilice en este proyecto.

--- Desktop/sus/Java (programas)/Chihuahua/src/main/java/mx/itson/chihuahua/business/Temperatura.java
+++ Desktop/sus/Java (programas)/Chihuahua/src/main/java/mx/itson/chihuahua/business/Temperatura.java
@@ -15,6 +15,12 @@ public class Temperatura
         return centigrados * 1.8 + 32;
     }
     
+    /**
+     * Calcula el tiempo que tardaria un carro en recorrer cierta distancia.
+     * @param distancia Distancia que recorrera el carro, son metros.
+     * @param velocidad Velocidad constante a la que viaja el carro, en kilometros por hora, el programa los pasa a metros por segundo.
+     * @return El tiempo que le tomo al carro recorrer esa distancia.
+     */
     public static double calcularTiempo(double distancia, double velocidad){
         return distancia / (velocidad / 3.6);
     }
